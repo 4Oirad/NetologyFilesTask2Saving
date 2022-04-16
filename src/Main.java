bb42764777d58e6ccd2e223be511df5ee5f334e7
Bair Arshaev <ars-bair@yandex.ru>
2022-04-17 00:54:50 +0300

    Edit code rev4

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,12 @@ public class Main {
         GameProgress gp1 = new GameProgress(100, 15, 7, 68.2);
         GameProgress gp2 = new GameProgress(32, 120, 15, 374.7);
         GameProgress gp3 = new GameProgress(87, 8, 10, 100.8);
-        File[] saveFiles = new File[] {saveGame(gp1, "C:\\Games\\savegames\\save1.dat"),
-        saveGame(gp2, "C:\\Games\\savegames\\save2.dat"),
-        saveGame(gp3, "C:\\Games\\savegames\\save3.dat")};
-
-        zipFiles("C:\\Games\\savegames\\save.zip", saveGame(gp1, "C:\\Games\\savegames\\save2.dat"));
-
-
+        File[] saveFiles = new File[] {
+                saveGame(gp2, "C:\\Games\\savegames\\save2.dat"),
+                saveGame(gp3, "C:\\Games\\savegames\\save3.dat"),
+                saveGame(gp1, "C:\\Games\\savegames\\save1.dat")
+        };
+        zipFiles("C:\\Games\\savegames\\save.zip", saveFiles);
     }
 
     static File saveGame(GameProgress gp, String fileDir) {
@@ -26,18 +25,20 @@ public class Main {
         return new File(fileDir);
     }
 
-    static void zipFiles(String zipFileDir, File files) {
+    static void zipFiles(String zipFileDir, File[] files) {
 
-        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFileDir));
-        FileInputStream fis = new FileInputStream(files.getAbsolutePath())) {
-            ZipEntry zipEntry = new ZipEntry(files.getName());
-            zos.putNextEntry(zipEntry);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zos.write(buffer);
-            zos.closeEntry();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFileDir))) {
+                for (File file : files) {
+                    FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+                    ZipEntry zipEntry = new ZipEntry(file.getName());
+                    zos.putNextEntry(zipEntry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zos.write(buffer);
+                    zos.closeEntry();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 }
