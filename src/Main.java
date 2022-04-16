1bccafdb895af69c9365183933a64911b643fd73
Bair Arshaev <ars-bair@yandex.ru>
2022-04-17 00:32:30 +0300

    Edit code rev3

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,29 @@ public class Main {
         GameProgress gp1 = new GameProgress(100, 15, 7, 68.2);
         GameProgress gp2 = new GameProgress(32, 120, 15, 374.7);
         GameProgress gp3 = new GameProgress(87, 8, 10, 100.8);
-        saveGame(gp1, "C:\\Games\\savegames\\save1.dat");
-        saveGame(gp2, "C:\\Games\\savegames\\save2.dat");
-        saveGame(gp3, "C:\\Games\\savegames\\save3.dat");
-        zipFiles("C:\\Games\\savegames\\save1.zip", "C:\\Games\\savegames\\save1.dat");
+        File[] saveFiles = new File[] {saveGame(gp1, "C:\\Games\\savegames\\save1.dat"),
+        saveGame(gp2, "C:\\Games\\savegames\\save2.dat"),
+        saveGame(gp3, "C:\\Games\\savegames\\save3.dat")};
+
+        zipFiles("C:\\Games\\savegames\\save.zip", saveGame(gp1, "C:\\Games\\savegames\\save2.dat"));
 
 
     }
 
-    static void saveGame(GameProgress gp, String fileDir) {
+    static File saveGame(GameProgress gp, String fileDir) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileDir))) {
             oos.writeObject(gp);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return new File(fileDir);
     }
 
-    static void zipFiles(String zipFileDir, String filesDir) {
+    static void zipFiles(String zipFileDir, File files) {
+
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFileDir));
-        FileInputStream fis = new FileInputStream(filesDir)) {
-            ZipEntry zipEntry = new ZipEntry("save1.dat");
+        FileInputStream fis = new FileInputStream(files.getAbsolutePath())) {
+            ZipEntry zipEntry = new ZipEntry(files.getName());
             zos.putNextEntry(zipEntry);
             byte[] buffer = new byte[fis.available()];
             fis.read(buffer);
